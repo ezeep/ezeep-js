2a9430aed78738e4c1eaaeb1ac8d71d3e12222f9
cgoern <christopher.goern@cortado.com>
2021-04-16 10:05:55 +0200

    Initial commit

---
+++ stencil.config.ts
@@ -0,0 +1,27 @@
+import { Config } from '@stencil/core'
+import { sass } from '@stencil/sass'
+
+export const config: Config = {
+  namespace: 'ezeep-js',
+  globalStyle: 'src/shared/global.scss',
+  globalScript: 'src/shared/global.ts',
+  plugins: [sass()],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements-bundle',
+    },
+    {
+      type: 'docs-readme',
+      footer: '',
+    },
+    {
+      type: 'www',
+      serviceWorker: null,
+      copy: [{ src: 'assets' }],
+    },
+  ],
+}
