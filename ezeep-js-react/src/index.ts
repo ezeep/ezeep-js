adab9e46996e0edf2628302443c4aef9c1b30115
hasch <hannes.schellinger@cortado.com>
2021-10-05 15:54:55 +0200

    moved angular and react component to root folder

---
+++ ezeep-js-react/src/index.ts
@@ -0,0 +1,23 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@ezeep/ezeep-js';
+
+
+
+export const CapDisplay = /*@__PURE__*/createReactComponent<JSX.CapDisplay, HTMLCapDisplayElement>('cap-display');
+export const CapHeading = /*@__PURE__*/createReactComponent<JSX.CapHeading, HTMLCapHeadingElement>('cap-heading');
+export const CapLabel = /*@__PURE__*/createReactComponent<JSX.CapLabel, HTMLCapLabelElement>('cap-label');
+export const CapParagraph = /*@__PURE__*/createReactComponent<JSX.CapParagraph, HTMLCapParagraphElement>('cap-paragraph');
+export const EzpAuth = /*@__PURE__*/createReactComponent<JSX.EzpAuth, HTMLEzpAuthElement>('ezp-auth');
+export const EzpBackdrop = /*@__PURE__*/createReactComponent<JSX.EzpBackdrop, HTMLEzpBackdropElement>('ezp-backdrop');
+export const EzpIcon = /*@__PURE__*/createReactComponent<JSX.EzpIcon, HTMLEzpIconElement>('ezp-icon');
+export const EzpIconButton = /*@__PURE__*/createReactComponent<JSX.EzpIconButton, HTMLEzpIconButtonElement>('ezp-icon-button');
+export const EzpPrinterSelection = /*@__PURE__*/createReactComponent<JSX.EzpPrinterSelection, HTMLEzpPrinterSelectionElement>('ezp-printer-selection');
+export const EzpPrinting = /*@__PURE__*/createReactComponent<JSX.EzpPrinting, HTMLEzpPrintingElement>('ezp-printing');
+export const EzpProgress = /*@__PURE__*/createReactComponent<JSX.EzpProgress, HTMLEzpProgressElement>('ezp-progress');
+export const EzpSelect = /*@__PURE__*/createReactComponent<JSX.EzpSelect, HTMLEzpSelectElement>('ezp-select');
+export const EzpTextButton = /*@__PURE__*/createReactComponent<JSX.EzpTextButton, HTMLEzpTextButtonElement>('ezp-text-button');
+export const EzpUserMenu = /*@__PURE__*/createReactComponent<JSX.EzpUserMenu, HTMLEzpUserMenuElement>('ezp-user-menu');
